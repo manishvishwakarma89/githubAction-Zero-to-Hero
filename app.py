0d45ea09e6bec382b1e1ea1309c2e11856c83aab
manishvishwakarma89 <manish.kumar.v@ramanujan.du.ac.in>
2026-03-15 20:02:47 +0530

    fixed index.html

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GitHub Actions Docker App!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    return render_template("index.html")
